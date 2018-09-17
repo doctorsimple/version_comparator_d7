110cf5face154a7c1142a37bc50f63289f0e64b5
doctorsimple <doctorsimple@gmail.com>
2018-09-17 14:46:30 -0500

    Fixing resize issues

--- version_comparator.js
+++ version_comparator.js
@@ -16,29 +16,35 @@ Drupal.behaviors.versionComparator = {
                 function(response) {
                     if (response[1] !== undefined)
                     {
+                        jQuery('select#translators').find('option:contains("'+tname+'")').remove();
                         var viewHtml = response[1].data;
                         $bookswrapper.find('#tilethrobber').remove();
                         $bookswrapper.append(viewHtml);
-                        //adjust widths
-                        var bookscount = jQuery('.view-ttc-book').length;
+                        jQuery('.vc-book-header').width('auto');
+                        jQuery('#bookswrapper .view-header').width('auto');
+                        var bookscount = jQuery('.view-version-comparator-book').length;
                         $bookswrapper.find('.view-version-comparator-book')
                         //Adjust row heights
                             .last().find('.views-row').each( function(i){
-                            $this = jQuery(this);
-                            var currheight = $this.outerHeight();
-                            var $samerows = jQuery('.views-row-' + (i+1), context);
-                            $samerows.each( function() {
-                                var $subthis = jQuery(this), myheight =  $subthis.outerHeight()
-                                if (myheight < currheight) {
-                                    $subthis.outerHeight(currheight);
-                                } else if (myheight > currheight) {
-                                    $this.outerHeight(myheight);
-                                    currheight = myheight;
+                                if ( bookscount == 1) {
+                                    return false;
                                 }
-
-                            });
+                            var $samerows = jQuery('.views-row-' + (i+1), context);
+                            jQuery($samerows).matchHeight();
                         });
-
+                        //Set headers
+                        jQuery('.vc-book-header').each( function(i) {
+                          var setwidth = jQuery(this).parents('.view-header').width();
+                          jQuery(this).width( setwidth);
+                          jQuery(this).parents('.view-header').width(setwidth);
+                        })
+                        jQuery('#bookswrapper .vc-book-header')
+                            .affix({
+                                offset: {
+                                    top: jQuery('#bookswrapper').offset().top, target: '.view-header'
+                                }
+                            }
+                        );
                     }
                 }
             )
